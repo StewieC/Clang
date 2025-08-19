34f4aaca26c10311f59bfadc5c7cacb62c83bf43
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-08-19 19:55:37 +0300

    beginning of C programming

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// If you are using VS Code, open the Command Palette (Ctrl+Shift+P), type "C/C++: Edit Configurations (UI)", and add the path to your compiler's include directory (e.g., C:\Program Files\mingw-w64\include) to "Include path".
